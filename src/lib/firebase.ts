659f0dc4c055aa2dabaa5318ffe0644667f3118d
mys3kings <kingsdavidfemi.15@gmail.com>
2026-04-10 20:04:33 +0100

    Refactor Firebase config to use hardcoded values

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDGyXvuNGlS-DfoCHy6_gNUudbAzkowStY",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "imperion-4abfe.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "imperion-4abfe",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "imperion-4abfe.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "533125885364",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:533125885364:web:6426956f6dd8942c6c56e1",
+  apiKey: "AIzaSyDGyXvuNGlS-DfoCHy6_gNUudbAzkowStY",
+  authDomain: "imperion-4abfe.firebaseapp.com",
+  projectId: "imperion-4abfe",
+  storageBucket: "imperion-4abfe.firebasestorage.app",
+  messagingSenderId: "533125885364",
+  appId: "1:533125885364:web:6426956f6dd8942c6c56e1"
 };
 
 const app = initializeApp(firebaseConfig);
